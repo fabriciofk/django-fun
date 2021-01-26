3effd05d797197ceb39a659559bbd0f19025842b
unknown <fabriciokarmierczak@gmail.com>
2021-01-26 14:18:03 -0300

    ativado

---
+++ awesome_website/users/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+
+from django.conf.urls import include, url
+from users.views import dashboard
+
+urlpatterns = [
+    url(r"^accounts/", include("django.contrib.auth.urls")),     url(r"^dashboard/", dashboard, name="dashboard"),
+]
